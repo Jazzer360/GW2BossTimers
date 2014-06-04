3405afea9ab47ad79a3cd9aac54faa804ac474ad
Derek Jass <jazzer36@gmail.com>
2014-06-04 15:41:15 -0500

    Key for user account.

--- GW2BossTimers/src/com/derekjass/gw2bosstimers/BossTimerApplication.java
+++ GW2BossTimers/src/com/derekjass/gw2bosstimers/BossTimerApplication.java
@@ -6,6 +6,7 @@ public class BossTimerApplication extends Application {
 
 	public static final String PREF_LAST_DISPLAY = "pref_last_display";
 	public static final String PREF_LAST_KILL_PREFIX = "pref_last_kill_";
+	public static final String PREF_ACCOUNT = "pref_account";
 
 	public static final long ONE_MINUTE = 60 * 1000;
 	public static final long FIFTEEN_MINS = 15 * ONE_MINUTE;
