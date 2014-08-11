b6d6e9ff2d209201b0c22db4f5edc2bc28666aa8
Derek Jass <jazzer36@gmail.com>
2014-08-11 15:37:40 -0500

    Adding banner removal functionality with in-app billing.

--- GW2BossTimers/src/com/derekjass/gw2bosstimers/MainActivity.java
+++ GW2BossTimers/src/com/derekjass/gw2bosstimers/MainActivity.java
@@ -1,7 +1,10 @@
 package com.derekjass.gw2bosstimers;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
@@ -10,19 +13,70 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBar.Tab;
 import android.support.v7.app.ActionBar.TabListener;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends ActionBarActivity {
+import com.derekjass.iabhelper.PurchaseStateFragment;
+import com.derekjass.iabhelper.PurchaseStateFragment.ProductType;
+import com.derekjass.iabhelper.PurchaseStateFragment.PurchaseState;
+import com.derekjass.iabhelper.SimplePurchaseStateFragment.PurchaseStateListener;
+import com.derekjass.iabhelper.SimplePurchaseStateFragment;
+
+public class MainActivity extends ActionBarActivity implements
+		PurchaseStateListener {
+
+	private static final String BANNER_PRODUCT_ID = "android.test.purchased";
+	private static final String BANNER_FRAGMENT_TAG = "ad_fragment";
+	private static final int BANNER_REQUEST_CODE = 0;
 
 	private ViewPager mViewPager;
+	private View mDonationBanner;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mDonationBanner = findViewById(R.id.donation_banner);
+		setupBannerClickListener();
+		addPurchaseStateFragment();
 
 		setupPager();
 
 		setupActionBar();
+	}
+
+	private void setupBannerClickListener() {
+		mDonationBanner.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				getPurchaseStateFragment().purchaseProduct(BANNER_REQUEST_CODE);
+			}
+		});
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == Activity.RESULT_OK) {
+			switch (requestCode) {
+			case BANNER_REQUEST_CODE:
+				getPurchaseStateFragment().handleActivityResult(data);
+				break;
+			}
+		}
+	}
+
+	private void addPurchaseStateFragment() {
+		FragmentManager fm = getSupportFragmentManager();
+		FragmentTransaction ft = fm.beginTransaction();
+		ft.add(SimplePurchaseStateFragment.newInstance(BANNER_PRODUCT_ID,
+				ProductType.MANAGED_PRODUCT), BANNER_FRAGMENT_TAG);
+		ft.commit();
+	}
+
+	private PurchaseStateFragment getPurchaseStateFragment() {
+		Fragment f = getSupportFragmentManager().findFragmentByTag(
+				BANNER_FRAGMENT_TAG);
+		return (PurchaseStateFragment) f;
 	}
 
 	private void setupPager() {
@@ -50,12 +104,10 @@ public class MainActivity extends ActionBarActivity {
 
 		mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
 			@Override
-			public void onPageScrollStateChanged(int state) {
-			}
+			public void onPageScrollStateChanged(int state) {}
 
 			@Override
-			public void onPageScrolled(int pos, float offset, int pixels) {
-			}
+			public void onPageScrolled(int pos, float offset, int pixels) {}
 
 			@Override
 			public void onPageSelected(int position) {
@@ -67,8 +119,7 @@ public class MainActivity extends ActionBarActivity {
 	private void setupActionBar() {
 		TabListener tabListener = new TabListener() {
 			@Override
-			public void onTabReselected(Tab tab, FragmentTransaction ft) {
-			}
+			public void onTabReselected(Tab tab, FragmentTransaction ft) {}
 
 			@Override
 			public void onTabSelected(Tab tab, FragmentTransaction ft) {
@@ -76,8 +127,7 @@ public class MainActivity extends ActionBarActivity {
 			}
 
 			@Override
-			public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-			}
+			public void onTabUnselected(Tab tab, FragmentTransaction ft) {}
 		};
 
 		ActionBar actionBar = getSupportActionBar();
@@ -90,5 +140,19 @@ public class MainActivity extends ActionBarActivity {
 				.setTabListener(tabListener));
 		actionBar.setDisplayShowHomeEnabled(false);
 		actionBar.setDisplayShowTitleEnabled(false);
+	}
+
+	@Override
+	public void onPurchaseStateChanged(String productId, PurchaseState state) {
+		switch (state) {
+		case NOT_PURCHASED:
+		case UNKNOWN:
+			mDonationBanner.setVisibility(View.VISIBLE);
+			break;
+		case PURCHASED:
+			mDonationBanner.setVisibility(View.GONE);
+			break;
+		default:
+		}
 	}
 }
